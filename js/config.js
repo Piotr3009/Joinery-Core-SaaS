72e67784bbba38efd5856d8de8c02886840a2d69
Piotr3009 <piotr.t@skylonconstruction.com>
2026-01-23 00:36:10 +0000

    Delete config.js

--- js/config.js
+++
@@ -1,7 +0,0 @@
-// Joinery Core SaaS - Configuration
-// API URL for backend communication
-
-const API_URL = 'https://joinerycore.com';
-
-// Note: supabaseClient is now provided by api-client.js
-// No Supabase keys needed in frontend anymore!
